868165c2ad56ea68c3be2c25debaf5b9f509966e
Alexander <gwartneydev@gmail.com>
2018-01-04 04:07:43 -0800

    Added comments to the inheritance example/Removed old example
    Finished adding in comments the inhereitance example. To explain the concepts that are happening with in the code.

--- Deep_Dive_Of_inheritance/Inhertiance.js
+++ Deep_Dive_Of_inheritance/Inhertiance.js
@@ -1,30 +1,63 @@
 
 
-// I have a constructor named BigDog that takes in 2 parameters
+//constructor named BigDog that takes in 2 parameters
 function BigDog(breed,age) {
 	this.breed = breed
     this.age = age
 }
 
+//constructor named littleDog that takes in 3 parameters
 function littleDog(breed,age,size){
+    /*Call the constructor BigDog and bind the values of its 
+    this to the littleDogs empty object.
+    Then pass in the arguments to be set
+    add in a extra size value to the empty object
+    */
     BigDog.call(this,breed,age);
-    this.size = size
+    //Add in one other specific value
+    this.size = size;
 }
 
+//Add animal info function to the prototype object
 BigDog.prototype.animalInfo = function () {
-  var animalBreed = `I am a ${this.breed} , ${this.age}`;
-  console.log(animalBreed, this);
+   
+  /*
+  The this in side the .prototype obj is set to what 
+  ever create the create the object when we called the new keyword
+  you can look at as the same thing small_dog.bred ect....
+  */
+
+  var animalBreed = `I am a ${this.breed} ${this.age} ${this.size}`;
+  console.log(animalBreed);
 }
 
+/*
+This now gets rid of littleDog 
+Constructor creates another empty object. That is then linked back to the bigDog prototyoe obj
+*/
+littleDog.prototype = Object.create(BigDog.prototype);
+
+
+//Then add the function getsmall dog function to the newly created linked object
 littleDog.prototype.getsmalldog = function(){
-  var smallBreed = `I am a ${this.breed} , ${this.age} , ${this.size}`;
-  console.log(smallBreed, this);
+ /*
+   The this will be again bound to what ever it was created with 
+   when calling the new keyword In this case since there is know 
+   animalInfo on this specific object it will then
+   look up the prototye chain to find the function and invoke it
+  */
+    return this.animalInfo();
 }
 
-// I create an object from my constructor and give it the 2 parameters
-ralph_dog = new BigDog("german shepard",24);
+
+/*
+I create an object from my constructor and give it the parameters
+ It then does 4 things to se evrey thing else in motion
+ 1:create a new empty object, 2:bind the this value of the constructor to this new empty object
+ 3:link the empty object to the prototype object.
+ 4:return the final object unless one is defined specificly in the constructor
+*/
+
 small_dog = new littleDog("chuwawa",25,"small");
 
-ralph_dog.animalInfo();
 small_dog.getsmalldog();
-
